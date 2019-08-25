0602f92cebf42eebc458ac3ffff0b23f8d3755ce
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-08-24 23:49:14 -0400

    Added a bit of detail to the connection test controller
    Signed-off-by: Ryan Cloherty <rcloherty@rocketsoftware.com>

--- src/main/java/io/github/incplusplus/thermostat/server/ConnectionTestController.java
+++ src/main/java/io/github/incplusplus/thermostat/server/ConnectionTestController.java
@@ -1,5 +1,6 @@
 package io.github.incplusplus.thermostat.server;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ public class ConnectionTestController
 	private static final String template = "Hello, %s";
 	private final AtomicLong counter = new AtomicLong();
 	
+	@ApiOperation(value = "Says hello back to you", notes = "Change the name parameter for a personalized greeting!")
 	@GetMapping("/hello-world")
 	@ResponseBody
 	public Greeting sayHello(@RequestParam(name="name", required = false, defaultValue = "Stranger") String name)
--- src/main/java/io/github/incplusplus/thermostat/server/Greeting.java
+++ src/main/java/io/github/incplusplus/thermostat/server/Greeting.java
@@ -1,8 +1,11 @@
 package io.github.incplusplus.thermostat.server;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Greeting
 {
 	private final long id;
+	@ApiModelProperty(example = "Hello, Stranger")
 	private final String content;
 	
 	public Greeting(long id, String content)
