4502b429cffd30bec80e4c67934a250c72c4f526
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-09-08 21:52:05 -0400

    Add PrivilegeRepository

---
+++ src/main/java/io/github/incplusplus/thermostat/persistence/repositories/PrivilegeRepository.java
@@ -0,0 +1,15 @@
+package io.github.incplusplus.thermostat.persistence.repositories;
+
+import io.github.incplusplus.thermostat.persistence.model.Privilege;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.UUID;
+
+public interface PrivilegeRepository extends MongoRepository<Privilege, ObjectId>
+{
+	Privilege findByName(String name);
+	
+	@Override
+	void delete(Privilege privilege);
+}
